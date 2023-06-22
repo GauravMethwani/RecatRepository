84d0c7f81d75612e30cdacda106c3d7e2dcd0869
gaurav <gmethwani365@gmail.com>
2023-06-22 03:31:06 -0700

    ReactRepository

---
+++ my-app/src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+
+function App() {
+  return (
+  <>
+   <header>
+      <ul>
+       <li>HOME</li>
+        <li>ABOUT</li>
+        <li>CONTACT</li>
+        <li>SERVICE</li>
+      </ul>
+   </header>
+   <section>
+    <h1>Welcome To React App</h1>
+   </section>
+   </>
+  );
+}
+
+export default App;
